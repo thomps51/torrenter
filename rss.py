95275b7c343e11f3a14cc42275ea2b98d8f91788
thomps51 <thomps51@illinois.edu>
2015-11-17 22:08:51 +0000

    tabs->spaces, debug statments

--- rss.py
+++ rss.py
@@ -6,35 +6,49 @@ import filer
 import torrenter
 
 def Month(month):
-	months = ["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"]
-	mon = months.index(month)
-	return mon + 1
+    months = ["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"]
+    mon = months.index(month)
+    return mon + 1
 def getDateRSS(rssFeed , index):
     if index >= len(rssFeed.entries) or len(rssFeed.entries[0].published) < 26:
         print "Why did this happen? Index too large! Malformed rssFeed? Debug info:"
         print "Index = " + str(index)
         print "size of entries = " + len(rssFeed.entries)
         print "size of published = " + len(rssFeed.entries[0].published)
-	date  = rssFeed.entries[index].published[5:7]
-	month = rssFeed.entries[index].published[8:11]
-	year  = rssFeed.entries[index].published[12:16]
-	
-	t1 = rssFeed.entries[index].published[17:19]
-	t2 = rssFeed.entries[index].published[20:22]
-	t3 = rssFeed.entries[index].published[23:25]
-	
-	return datetime(int(year),int(Month(month)),int(date),int(t1),int(t2),int(t3))
+        print rssFeed.entries[0].published
+        print rssFeed.entries[index].published 
+    date  = rssFeed.entries[index].published[5:7]
+    month = rssFeed.entries[index].published[8:11]
+    year  = rssFeed.entries[index].published[12:16]
+    
+    #print date+month+year
+    
+    t1 = rssFeed.entries[index].published[17:19]
+    t2 = rssFeed.entries[index].published[20:22]
+    t3 = rssFeed.entries[index].published[23:25]
+   
+    dateRSS=datetime(int(year),int(Month(month)),int(date),int(t1),int(t2),int(t3))
+
+    if dateRSS is None:
+        print "Debug info:"
+        print "Index = " + str(index)
+        print "size of entries = " + len(rssFeed.entries)
+        print "size of published = " + len(rssFeed.entries[0].published)
+        print rssFeed.entries[0].published
+        print rssFeed.entries[index].published 
+ 
+    return dateRSS
 def getDateFile():
-	with open('time.txt', 'r') as f:
-	          sDate = f.readline()
-	
-	year  = int(sDate[0:4])
-	month = int(sDate[5:7])
-	day   = int(sDate[8:10])
-	t1    = int(sDate[11:13])
-	t2    = int(sDate[14:16])
-	t3    = int(sDate[17:19])
-	return datetime(int(year),int(month),int(day),int(t1),int(t2),int(t3))
+    with open('time.txt', 'r') as f:
+        sDate = f.readline()
+    
+    year  = int(sDate[0:4])
+    month = int(sDate[5:7])
+    day   = int(sDate[8:10])
+    t1    = int(sDate[11:13])
+    t2    = int(sDate[14:16])
+    t3    = int(sDate[17:19])
+    return datetime(int(year),int(month),int(day),int(t1),int(t2),int(t3))
 
 def getRSSfeed():
     #url = "http://showrss.info/rss.php?user_id=248153&hd=1&proper=1&raw=true" 
@@ -63,23 +77,23 @@ def line_prepender(filename, line):
         f.write(line.rstrip('\r\n') + '\n' + content)
 
 def update(rssFeed, tempDir, libBaseDir):
-	#download latest
-	updateTime  = getDateRSS(rssFeed,0)
-	magnetLinks = []
-	showTitles  = []
-	updated	    = False
-	for i in range(len(rssFeed.entries)-1,-1,-1): # start at oldest entry, work forward
+    #download latest
+    updateTime  = getDateRSS(rssFeed,0)
+    magnetLinks = []
+    showTitles  = []
+    updated	    = False
+    for i in range(len(rssFeed.entries)-1,-1,-1): # start at oldest entry, work forward
 
-		feedDate = getDateRSS(rssFeed, i)
-		if getDateFile() < feedDate:
-	
-			# check from last item in RSS feed, comparing titles
-			# if saved title not found, start from last item in feed
-			print "Found Update: " + rssFeed.entries[i].title
-			magnetLinks.append(rssFeed.entries[i].link)
-			showTitles.append(getShowTitle(rssFeed,i))
-			updated = True
-	f = open('time.txt', 'w')
-	f.write(str(updateTime))
-	f.close()
-	return updated, magnetLinks, showTitles
+        feedDate = getDateRSS(rssFeed, i)
+        #print feedDate
+        if getDateFile() < feedDate:
+    	    # check from last item in RSS feed, comparing titles
+    	    # if saved title not found, start from last item in feed
+    	    print "Found Update: " + rssFeed.entries[i].title
+    	    magnetLinks.append(rssFeed.entries[i].link)
+    	    showTitles.append(getShowTitle(rssFeed,i))
+    	    updated = True
+    f = open('time.txt', 'w')
+    f.write(str(updateTime))
+    f.close()
+    return updated, magnetLinks, showTitles
